e8c86249d0612cc69574f144a8580ce126fd717a
zhangyue <zhangyue19961218@gmail.com>
2020-11-30 23:19:23 -0300

    [Update] update

--- scripts/start.js
+++ scripts/start.js
@@ -1,14 +1,7 @@
 'use strict'
 
 // Do this as the first thing so that any code reading it knows the right env.
-const DEV = false; //(process.env.NODE_ENV || 'development') === 'development'
-if (DEV) {
-  process.env.BABEL_ENV = 'development'
-  process.env.NODE_ENV = 'development'
-} else {
-  process.env.BABEL_ENV = 'production'
-  process.env.NODE_ENV = 'production'
-}
+const DEV = (process.env.NODE_ENV || 'development') === 'development'
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
