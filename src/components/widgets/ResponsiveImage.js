d6e5d449f25fed1e99c4f2c3cd64db2a18701eee
zhangyue <zhangyue19961218@gmail.com>
2021-03-22 21:53:07 +0200

    [Fix]: world-water-day image issue again

--- src/components/widgets/ResponsiveImage.js
+++ src/components/widgets/ResponsiveImage.js
@@ -84,13 +84,14 @@ class ResponsiveImage extends Component {
           typeof window !== 'undefined' && window.location.href.split('/')[4]
       })
     }
+    console.log(this.state.selectURL, '-----------ResponsiveImage:');
 
     return <div className={cn(classes.container, { [classes.fixedAspect]: aspect }, className)} ref={onRef}>
       {/* {blur && <img key='blur' src={blur.data ? blur.data : blur.url} className={classes.imageBlur} alt={mainImage ? mainImage.alt : ''} />} */}
       <picture>
         {/* <!--[if IE 9]><video style="display: none;"><![endif]--> */}
         {sources}
-        {this.state.selectURL === 'water-world-day' ? <ImageElement1 /> : <ImageElement /> }
+        {this.state.selectURL === 'world-water-day' ? <ImageElement1 /> : <ImageElement /> }
         {/* <!--[if IE 9]></video><![endif]--> */}
       </picture>
       {children}
