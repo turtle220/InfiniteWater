bfcb25acc23bfb10a9909f75462539bc68f8a951
zhangyue <zhangyue19961218@gmail.com>
2021-04-11 22:44:54 +0300

    [Fix]: remove newfooter.css

--- src/server/middleware/render.js
+++ src/server/middleware/render.js
@@ -91,7 +91,6 @@ async function render ({ clientStats, serverStats }, req, res) {
             '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">' +
             '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">' +
             '<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">' +
-            '<link rel="stylesheet" href="./NewFooter.css">' +
             '<meta name="msapplication-TileColor" content="#ffc40d">' +
             '<meta name="theme-color" content="#000000">' +
             helmet.title.toString() +
